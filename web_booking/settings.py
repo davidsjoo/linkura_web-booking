31d5c6251cab12115cd71b62171f7d113df13796
davidsjoo <davidsjoo@gmail.com>
2015-05-07 12:46:57 +0200

    Update settings.py

--- web_booking/settings.py
+++ web_booking/settings.py
@@ -88,9 +88,9 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'linkuramailtest@gmail.com'
+EMAIL_HOST = ''
+EMAIL_HOST_USER = ''
 EMAIL_PORT = 587
-EMAIL_HOST_PASSWORD = 'g3ghhi5623hu'
+EMAIL_HOST_PASSWORD = ''
 
 
